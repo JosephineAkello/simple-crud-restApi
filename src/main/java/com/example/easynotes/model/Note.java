3a8f2fbeb7cf744b5383f512d29f7a154d73203a
Maureen Josephine <Maureen Josephine>
2019-11-21 17:23:21 +0300

    create notes model

---
+++ src/main/java/com/example/easynotes/model/Note.java
@@ -0,0 +1,24 @@
+package com.example.easynotes.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "notes")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+
+public class Note implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    
+}
